eeeae8f7b9d60df93965652679382e8abd25907c
Gaurav Sanjeev Pathak <114300073+gaurav9359@users.noreply.github.com>
2024-02-13 06:56:49 +0530

    Few more comments added with proper structure

--- firefly_automation/firefly_automation.js
+++ firefly_automation/firefly_automation.js
@@ -1,5 +1,5 @@
 /**
- * Problem statement was to implement the Jugnoo Problem
+ * Problem statement was to implement the Firefly Problem
  * Requirement was to take the grid of 30 height x 50 width
  * then we have to take a "*" inside that grid and move the
  * star in all the 9 direction of grid randomly and ensure that
@@ -41,26 +41,35 @@ var choiceArray = [1, 0, -1];
 function print_star(gHeight, gWidth,xDirection,yDirection) {
 
     //if given parameters are not integer then return 
-    if(!(isInteger(gHeight) && isInteger(gWidth) && isInteger(xDirection) && isInteger(yDirection))){
+    if(!(isInteger(gHeight) && isInteger(gWidth) && isInteger(xDirection) 
+    && isInteger(yDirection))){
         console.log("given arguments are not valid")
         return 
     }
 
-    //Logic to print boundary and "*"
+    //Logic to print boundary and "*" inside that boundary
     for (let i = 0; i < gHeight; i++) {
         for (let j = 0; j < gWidth; j++) {
-            if (i === 0 && j === 0 || i === 0 && j === gWidth - 1 || i === gHeight - 1 && j === 0 || i === gHeight - 1 && j === gWidth - 1) {
+            //To print "+" in top-left,top-right, 
+            //bottom-left and bottom-right corner
+            if (i === 0 && j === 0 || i === 0 && j === gWidth - 1 
+                || i === gHeight - 1 && j === 0 || i === gHeight - 1 
+                && j === gWidth - 1) {
                 process.stdout.write("+");
             } 
+            // To print the horizontal boundary
             else if (i === 0 || i === gHeight - 1) {
                 process.stdout.write("_");
             } 
+            //To print the vertical boundary
             else if (j === 0 || j === gWidth - 1) {
                 process.stdout.write("|");
             } 
+            // To print the star inside the boundary
             else if (i === yDirection && j === xDirection) {
                 process.stdout.write("*");
             } 
+            // Fill the rest of the space with " "
             else {
                 process.stdout.write(" ");
             }
@@ -68,27 +77,27 @@ function print_star(gHeight, gWidth,xDirection,yDirection) {
         console.log();
         
     }
-    
-    
-    
 }
 
 /**
- * Logic to update the location of star and rendering the screen in every 8 millisecond
+ * Logic to update the location of star and rendering the screen 
+ * in every 8 millisecond
  */
 setInterval(() => {
     console.clear()
+    // Add 0 or 1 or -1 in x and y direction randomly
     let toAdd_x=choiceArray[Math.floor(Math.random() * choiceArray.length)]
     let toAdd_y=choiceArray[Math.floor(Math.random() * choiceArray.length)]
     xDirection+=toAdd_x
     yDirection+=toAdd_y
+    //check if the firefly is going outside the boundary, undo the changes
     if(xDirection<=0 || xDirection>=gHeight-1){
         xDirection-=toAdd_x
     }
     if(yDirection<=0 || yDirection>=gHeight-1){
         yDirection-=toAdd_y
     }  
-
+    // run the function print_star in every 800 millisecond
     print_star(gHeight, gWidth,xDirection,yDirection);
     
 }, 800);
